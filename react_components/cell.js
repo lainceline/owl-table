213fb398137dd70bfc5c31087629176f349b6eb0
James Asher <jamesa@focusschoolsoftware.com>
2014-12-01 15:01:13 -0500

    Well, swiftbox "works".  This is as good as its gonna get for right now

--- react_components/cell.js
+++ react_components/cell.js
@@ -34,6 +34,9 @@ var OwlCell = React.createClass({
 		);
 	},
 	componentDidUpdate: function () {
+
+		var self = this;
+
 		if (this.props.focusedCell !== false) {
 			this.props.focusedCell.find('input').focus();
 		}
@@ -42,6 +45,20 @@ var OwlCell = React.createClass({
 
 		if (swiftboxes.length > 0) {
 			swiftboxes.swiftbox();
+
+			swiftboxes.each(function (index, box) {
+
+				$(box).off('change');
+
+				$(box).on('change', function (event) {
+					var node = $(self.getDOMNode());
+					var props = self.props;
+
+					var val = node.find('.swift-box-hidden-input').val();
+
+					node.trigger('owlTableUpdated', [props.column, props.row, val]);
+				});
+			});
 		}
 	}
 });
