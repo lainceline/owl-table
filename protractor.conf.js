e0e0488e5bdc05e961fd498d5d3c106dcf96eda2
James Asher <jamesa@focusschoolsoftware.com>
2014-12-01 11:10:11 -0500

    working on protractor support

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,19 +1,17 @@
 // An example configuration file.
 
-require('coffee-script');
-
 exports.config = {
-  directConnect: true,
+  directConnect: false,
   allScriptsTimeout: 11000,
 
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
     'browserName': 'chrome'
   },
-
+  seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
   // Spec patterns are relative to the current working directly when
   // protractor is called.
-  specs: ['tests/e2e/**/*_spec.coffee'],
+  specs: ['tests/e2e/**/*_spec.js'],
 
   framework: 'jasmine',
 
--- tests/e2e/table_spec.coffee
+++ tests/e2e/table_spec.coffee
@@ -1,6 +1,13 @@
+
 describe 'E2E: owl-table with demo page as testbed', ->
 	ptor = {}
+	By = null
 
 	beforeEach ->
-		browser.get('http://localhost:9000')
+		browser.get('http://localhost:4444')
 		ptor = protractor.getInstance()
+		By = protractor.By
+
+	describe 'owl-table', ->
+		it 'should initially display a loading indicator', ->
+			expect(element(By.css('.owl-ajax-loading')).element(By.tagName('p')).getText()).toBe('Loading data...')
