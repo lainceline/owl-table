5a4230d701e237b12821a28d8d59087c26b9fe79
James Asher <jamesa@focusschoolsoftware.com>
2014-12-17 09:06:16 -0500

    Fixing Sauce Labs again

--- Gulpfile.js
+++ Gulpfile.js
@@ -207,7 +207,7 @@ gulp.task('karma-sauce', function (callback) {
 });
 
 gulp.task('test-nightwatch', shell.task(['nightwatch']));
-gulp.task('nightwatch-sauce', shell.task(['nightwatch -e saucelabs-chrome, saucelabs-ie, saucelabs-ff']));
+gulp.task('nightwatch-sauce', shell.task(['nightwatch -e saucelabsChrome, saucelabsIe, saucelabsFf']));
 
 gulp.task('test-saucelabs', function (callback) {
 	runSequence('build', 'karma-sauce', 'coffee-tests', 'nightwatch-sauce', callback);
