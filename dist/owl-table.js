482e25e7cb4a42dedc5b2cf720146186611604cd
James Asher <jamesa@focusschoolsoftware.com>
2014-12-29 09:48:44 -0500

    mm/dd/yyyy for datepicker, for the 1000th time

--- dist/owl-table.js
+++ dist/owl-table.js
@@ -125,7 +125,7 @@ var OwlInput = React.createClass({
 				break;
 			case 'date':
 				input =
-					React.createElement("input", {className: "owl-input", defaultValue: props.value, 'data-date-format': "dd/mm/yyyy", 'data-provide': "datepicker"});
+					React.createElement("input", {className: "owl-input", defaultValue: props.value, 'data-date-format': "mm/dd/yyyy", 'data-provide': "datepicker"});
 				break;
 			case 'time':
 				input = React.createElement("input", {className: "owl-input", type: "time", onChange: self.handleSpecialFields, defaultValue: props.value});
