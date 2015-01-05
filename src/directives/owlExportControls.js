8f558468db41878f50c11ae499e278c30aa94c98
James Asher <onijim@gmail.com>
2015-01-05 16:58:04 -0500

    Dont put data into the CSV if there's no column for that field

--- src/directives/owlExportControls.js
+++ src/directives/owlExportControls.js
@@ -23,14 +23,18 @@
 					return $scope.data.map(function (datum, index) {
 						_.forOwn(datum, function (value, key) {
 							var column = _.where(columns, {'field': key});
-							column = _.first(column) || {};
-							if (typeof column.options !== 'undefined') {
-								var option = _.where(column.options, {'value': value});
-								option = _.first(option) || {};
-								if (typeof option.text !== 'undefined') {
-									// do this jquery thing to strip out any html
-									datum[key] = $(option.text).text();
+							column = _.first(column);
+							if (typeof column !== 'undefined') {
+								if (typeof column.options !== 'undefined') {
+									var option = _.where(column.options, {'value': value});
+									option = _.first(option) || {};
+									if (typeof option.text !== 'undefined') {
+										// do this jquery thing to strip out any html
+										datum[key] = $(option.text).text();
+									}
 								}
+							} else {
+								delete datum[key];
 							}
 						});
 
